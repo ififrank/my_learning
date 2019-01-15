b1653e595c57e84016e7d0fe4d9b0a935e826e04
Ifeanyi Franklin Ike <ifeanyi.franklin@ehealthnigeria.org>
2019-01-15 16:54:49 +0100

    This is the pyhton version

---
+++ code.py
@@ -0,0 +1,6 @@
+first = 'Ifeanyi'
+last = 'Ike'
+print('My first and last names are {} and {} respectivley' .format(f = 'first', l = 'last'));
+print(f'My first name is {first}');
+my_dictionary = {'French pepper': 2000, 'Potatoe rice': 4000, 'drinks':{'Fanta':150, 'Coke': 150, 'Maltina':200}}
+status = (f'Now, I am progressing{Ifeanyi}')
